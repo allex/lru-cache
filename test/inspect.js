90fe4d46d7c1c11045b12ebd2829cec845eb1f4a
Allex Wang <allex.wxn@gmail.com>
2018-10-12 17:01:06 +0800

    Fix inspect test.

--- test/inspect.js
+++ test/inspect.js
@@ -3,6 +3,95 @@ var util = require('util')
 var t = require('tap')
 var LRU = require('../')
 
+// get LRU default naiveLength
+const naiveLength = (new LRU()).lengthCalculator
+
+function isStale (self, hit) {
+  if (!hit || (!hit.maxAge && !self.maxAge)) {
+    return false
+  }
+  var stale = false
+  var diff = Date.now() - hit.now
+  if (hit.maxAge) {
+    stale = diff > hit.maxAge
+  } else {
+    stale = self.maxAge && (diff > self.maxAge)
+  }
+  return stale
+}
+
+LRU.prototype.inspect = function (n, opts) {
+  var str = 'LRUCache {'
+  var extras = false
+
+  var as = this.allowStale
+  if (as) {
+    str += '\n  allowStale: true'
+    extras = true
+  }
+
+  var max = this.max
+  if (max && max !== Infinity) {
+    if (extras) {
+      str += ','
+    }
+    str += '\n  max: ' + util.inspect(max, opts)
+    extras = true
+  }
+
+  var maxAge = this.maxAge
+  if (maxAge) {
+    if (extras) {
+      str += ','
+    }
+    str += '\n  maxAge: ' + util.inspect(maxAge, opts)
+    extras = true
+  }
+
+  var lc = this.lengthCalculator
+  if (lc && lc !== naiveLength) {
+    if (extras) {
+      str += ','
+    }
+    str += '\n  length: ' + util.inspect(this.length, opts)
+    extras = true
+  }
+
+  var didFirst = false
+  this.dumpLru().forEach(function (item) {
+    if (didFirst) {
+      str += ',\n  '
+    } else {
+      if (extras) {
+        str += ',\n'
+      }
+      didFirst = true
+      str += '\n  '
+    }
+    var key = util.inspect(item.key).split('\n').join('\n  ')
+    var val = { value: item.value }
+    if (item.maxAge !== maxAge) {
+      val.maxAge = item.maxAge
+    }
+    if (lc !== naiveLength) {
+      val.length = item.length
+    }
+    if (isStale(this, item)) {
+      val.stale = true
+    }
+
+    val = util.inspect(val, opts).split('\n').join('\n  ')
+    str += key + ' => ' + val
+  })
+
+  if (didFirst || extras) {
+    str += '\n'
+  }
+  str += '}'
+
+  return str
+}
+
 var l = LRU()
 
 function inspect (str) {
